18597e64be5b988ec83f3a081c0da59baaa312e0
Vanessa Pereira <vnspereira11@gmail.com>
2023-01-23 18:13:19 -0300

    Add theme and global styles

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+    COLORS: {
+        BACKGROUND_900: "#1C1B1E",	
+        BACKGROUND_800: "#262529",
+        BACKGROUND_700: "#FF859B",
+        
+        PINK: "#FF859B",
+        PINK_LIGHT: "#F4EDE8",
+        
+        GRAY: "#312E38",
+        GRAY_LIGHT: "#948F99",
+        
+        WHITE: "#FFFFFF",
+    }
+  };
