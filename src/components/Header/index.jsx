ae42a1c6b39d1c58f4d7c61cc049e71ea2eea6c6
Vanessa Pereira <vnspereira11@gmail.com>
2023-02-03 20:41:35 -0300

    Fix signout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Container, Search, Profile, Logout } from "./styles";
 import { useAuth } from "../../hooks/auth";
 import { api } from "../../services/api";
@@ -5,6 +6,12 @@ import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
 
 export function Header({ children }) {
   const { signOut, user } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  };
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -21,7 +28,7 @@ export function Header({ children }) {
             <strong>{user.name}</strong>
             <img src={avatarUrl} alt={user.name} />
           </Profile>
-          <Logout onClick={signOut}>sair</Logout>
+          <Logout onClick={handleSignOut}>sair</Logout>
             
       </Container>
     );
